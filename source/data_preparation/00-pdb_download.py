e352e6a46bd7ad59e33e39260bd8c6b301e88ae4
byungukP <90356818+byungukP@users.noreply.github.com>
2024-02-23 17:05:54 -0600

    Update 00-pdb_download.py
    http not working to fetch PDB file from server

--- source/data_preparation/00-pdb_download.py
+++ source/data_preparation/00-pdb_download.py
@@ -24,8 +24,10 @@ in_fields = sys.argv[1].split('_')
 pdb_id = in_fields[0]
 
 # Download pdb 
-pdbl = PDBList(server='http://ftp.wwpdb.org')
+pdbl = PDBList()            # server='http://ftp.wwpdb.org': not working. so download as ent format first then change into pdb format
 pdb_filename = pdbl.retrieve_pdb_file(pdb_id, pdir=masif_opts['tmp_dir'],file_format='pdb')
+# Assuming 'pdb1a0g.ent' is the downloaded file
+#os.rename('pdb1a0g.ent', 'pdb1a0g.pdb')
 
 ##### Protonate with reduce, if hydrogens included.
 # - Always protonate as this is useful for charges. If necessary ignore hydrogens later.
