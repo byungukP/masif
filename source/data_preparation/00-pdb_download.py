98d4b11936996a386691618168f4b1a79c2aec2b
byungukP <90356818+byungukP@users.noreply.github.com>
2024-02-23 17:26:28 -0600

    Update 00-pdb_download.py
    update for PDBList()
    - either http or ftp not fetching pdb format directly from the server so have to os.rename the raw data to pdb format

--- source/data_preparation/00-pdb_download.py
+++ source/data_preparation/00-pdb_download.py
@@ -25,9 +25,18 @@ pdb_id = in_fields[0]
 
 # Download pdb 
 pdbl = PDBList()            # server='http://ftp.wwpdb.org': not working. so download as ent format first then change into pdb format
-pdb_filename = pdbl.retrieve_pdb_file(pdb_id, pdir=masif_opts['tmp_dir'],file_format='pdb')
-# Assuming 'pdb1a0g.ent' is the downloaded file
-#os.rename('pdb1a0g.ent', 'pdb1a0g.pdb')
+obsolete_pdbs = pdbl.get_all_obsolete()
+# check whether obsolete PDB ID or not
+if pdb_id in obsolete_pdbs:
+    pdb_filename = pdbl.retrieve_pdb_file(pdb_id, obsolete=True, pdir=masif_opts['tmp_dir'], file_format='pdb')
+    # pdb_filename = pdbl.retrieve_pdb_file(pdb_id, obsolete=True, pdir=masif_opts['tmp_dir'],file_format='pdb')
+else:
+    pdb_filename = pdbl.retrieve_pdb_file(pdb_id, pdir=masif_opts['tmp_dir'], file_format='pdb')
+    # pdb_filename = pdbl.retrieve_pdb_file(pdb_id, pdir=masif_opts['tmp_dir'],file_format='pdb')
+
+# Assuming 'pdb1a0g.ent' is the downloaded file when pdb_id is "1A0G"
+os.rename('{}/pdb{}.ent'.format(masif_opts["tmp_dir"], pdb_id.lower()), '{}/{}.pdb'.format(masif_opts["tmp_dir"], pdb_id))
+pdb_filename = '{}/{}.pdb'.format(masif_opts["tmp_dir"], pdb_id)
 
 ##### Protonate with reduce, if hydrogens included.
 # - Always protonate as this is useful for charges. If necessary ignore hydrogens later.
